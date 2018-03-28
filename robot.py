2e8b13a6cb60f4dfe63aa850e7842ced3e718462
lincolnmroth <lincoln@lroth.tech>
2018-03-28 13:37:00 -0400

    adds self

--- robot.py
+++ robot.py
@@ -7,19 +7,21 @@ import serial
 
 class Robot():
 
-    FRONT = 0
-    LEFT = 1
-    RIGHT = 2
-    FRONT_RIGHT = 3
-    FRONT_LEFT = 4
-    REAR_RIGHT = 5
-    REAR_LEFT = 6
-    LUX_CUTOFF = 50 #completely random
-    FLAME_LED = 18
-    START_LED = 23
+
     #SERIAL_PORT = serial.Serial('/dev/ttyUSB0', 9600, timeout=0)
 
     def __init__(self, left, right, versa):
+        self.FRONT = 0
+        self.LEFT = 1
+        self.RIGHT = 2
+        self.FRONT_RIGHT = 3
+        self.FRONT_LEFT = 4
+        self.REAR_RIGHT = 5
+        self.REAR_LEFT = 6
+        self.LUX_CUTOFF = 50 #completely random
+        self.FLAME_LED = 18
+        self.START_LED = 23
+
         self.left = left
         self.right = right
         self.versa = versa
@@ -30,12 +32,12 @@ class Robot():
         setupLeds()
 
     def setupLeds():
-        GPIO.setup(FLAME_LED, GPIO.OUT)
-        GPIO.setup(START_LED, GPIO.OUT)
+        GPIO.setup(self.FLAME_LED, GPIO.OUT)
+        GPIO.setup(self.START_LED, GPIO.OUT)
 
     def cleanupLeds():
-        GPIO.cleanup(FLAME_LED)
-        GPIO.cleannup(START_LED)
+        GPIO.cleanup(self.FLAME_LED)
+        GPIO.cleannup(self.START_LED)
 
     def goStraight(self, distance):
         pass
@@ -76,36 +78,36 @@ class Robot():
         pass
 
     def startLed(self, boolean):
-        GPIO.output(START_LED, boolean)
+        GPIO.output(self.START_LED, boolean)
 
     def flameLed(self, boolean):
-        GPIO.output(FLAME_LED, boolean)
+        GPIO.output(self.FLAME_LED, boolean)
 
     def readRoomLine(self):
         r, g, b, c = self.tcs.get_raw_data()
         print(Adafruit_TCS34725.calculate_lux(r, g, b))
-        return Adafruit_TCS34725.calculate_lux(r, g, b)>LUX_CUTOFF
+        return Adafruit_TCS34725.calculate_lux(r, g, b)>self.LUX_CUTOFF
 
     def readFront(self):
-        return self.sen.readSingleMetric(FRONT)
+        return self.sen.readSingleMetric(self.FRONT)
 
     def readLeft(self):
-        return self.sen.readSingleMetric(LEFT)
+        return self.sen.readSingleMetric(self.LEFT)
 
     def readRight(self):
-        return self.sen.readSingleMetric(RIGHT)
+        return self.sen.readSingleMetric(self.RIGHT)
 
     def readFrontLeft(self):
-        return self.sen.readSingleMetric(FRONT_LEFT)
+        return self.sen.readSingleMetric(self.FRONT_LEFT)
 
     def readFrontRight(self):
-        return self.sen.readSingleMetric(FRONT_RIGHT)
+        return self.sen.readSingleMetric(self.FRONT_RIGHT)
 
     def readRearRight(self):
-        return self.sen.readSingleMetric(REAR_RIGHT)
+        return self.sen.readSingleMetric(self.REAR_RIGHT)
 
     def raedRearLeft(self):
-        return self.sen.readSingleMetric(REAR_LEFT)
+        return self.sen.readSingleMetric(self.REAR_LEFT)
 
     def readAllUltras(self):
         return self.sen.readAllMetric()
